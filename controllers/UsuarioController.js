0d9c51072105f2baba674068c23ac3054c13a47d
Lincol Eulogio <2023280030@unheval.pe>
2024-03-08 22:33:39 -0500

    creamos el registro, editamos el usuari ocontroller agfregamos fuentes y css  y el js

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -1,9 +1,9 @@
 const formularioLogin = (req, res) => {
-  res.render("auth/login", { title: "Iniciar sesión" });
+  res.render("auth/login", { pagina: "Iniciar sesión" });
 };
 
 const formularioRegistro = (req, res) => {
-  res.render("auth/registro", { title: "Registro" });
+  res.render("auth/registro", { pagina: "Crear Cuenta" });
 };
 
 export { formularioLogin, formularioRegistro };
