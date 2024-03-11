0476903117ca4048c04d1493b86a3c846efad8cd
Lincol Eulogio <2023280030@unheval.pe>
2024-03-11 07:49:33 -0500

    actualizamos el .env de la conexion  a la base de datos

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
-import { Sequelize } from "sequelize";
+import  Sequelize from "sequelize";
 import dotenv from "dotenv";
 dotenv.config({path: '.env'});
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USUARIO, process.env.DB_PASSWORD ?? '', {
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USUARIO, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: "mysql",
   port: process.env.DB_PORT,
+  dialect: "mysql",
   define: {
     timestamps: false,
   },
