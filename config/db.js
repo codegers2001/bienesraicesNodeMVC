fa1bdf0c3f903ecac9865b2066ce5c76e0898c13
Lincol Eulogio <2023280030@unheval.pe>
2024-03-10 20:15:26 -0500

    instalamos dontenv para configurar el env de los datos de nuestra conexion ala base de datos

--- config/db.js
+++ config/db.js
@@ -1,9 +1,11 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config({path: '.env'});
 
-const sequelize = new Sequelize("bienesraicesnode", "root", "71916352", {
-  host: "localhost",
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USUARIO, process.env.DB_PASSWORD ?? '', {
+  host: process.env.DB_HOST,
   dialect: "mysql",
-  port: "3306",
+  port: process.env.DB_PORT,
   define: {
     timestamps: false,
   },
